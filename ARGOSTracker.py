5abf89bfcf174c7200bebfc260abcd3b91081a05
jamorris15 <54645512+jamorris15@users.noreply.github.com>
2019-10-01 10:56:41 -0400

    Create ARGOSTracker.py

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTracker.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Jannette Morris (jannette.morris@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
